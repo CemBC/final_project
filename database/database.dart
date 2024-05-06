f234b3826dad72602d5b90e5ff6ebfe7169b688e
CemBC <119740765+CemBC@users.noreply.github.com>
2024-05-06 11:58:55 +0300

    İmplementing database

---
+++ database/database.dart
@@ -0,0 +1,53 @@
+  import 'dart:async';
+  import 'package:path/path.dart';
+  import 'package:final_project/final_project/models/user_model.dart';
+  import 'package:sqflite/sqflite.dart';
+
+  class DatabaseService {
+    static final DatabaseService _databaseService = DatabaseService._internal();
+    factory DatabaseService() => _databaseService;
+    DatabaseService._internal();
+
+    static Database? _database;
+
+    Future<Database> get database async {
+      _database ??= await _initDatabase();
+      return _database!;
+    }
+
+    Future<Database> _initDatabase() async {
+      final databasePath = await getDatabasesPath();
+      final path = join(databasePath, 'user.db');
+
+      return await openDatabase(
+        path,
+        onCreate: _onCreate,
+        version: 1,
+      );
+    }
+
+
+    Future<void> _onCreate(Database db, int version) async {
+      await db.execute("CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT , username TEXT , password TEXT)"
+      );
+    }
+
+
+    Future<int> createUser(User user) async {
+      final Database db = await _initDatabase();
+      return db.insert('users' , user.toJson(),conflictAlgorithm: ConflictAlgorithm.replace);
+    }
+
+    Future<List<User>> users() async {
+      final db = await _databaseService.database;
+      final List<Map<String, dynamic>> maps = await db.query('user');
+      return List.generate(maps.length, (index) => User.fromJson(maps[index]));
+    }
+
+    Future<bool> isExist(String username, String password) async {
+      final db = await _databaseService.database;
+      final List<Map<String, dynamic>> result = await db.query('user', where: 'username = ? AND password = ?', whereArgs: [username, password]);
+      return result.isNotEmpty;
+    }
+
+  }
--- models/user_model.dart
+++ models/user_model.dart
@@ -2,21 +2,20 @@ import 'dart:convert';
 class User{
   final String username;
   final String password;
-  final int? id;
 
-  User({required this.username , required this.password , required this.id});
+  User({required this.username , required this.password });
 
 
   factory User.fromJson(Map<String, dynamic> json) => User(
     username: json["username"],
     password: json["password"],
-    id: json["id"],
+
   );
 
   Map<String, dynamic> toJson() => {
     "username": username,
     "password": password,
-    "id": id,
+
   };
 
 }
